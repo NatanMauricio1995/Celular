f667d56ef8792e831ff068849b300e1d474a3719
Natan Mauricio Santos <natanmauriciosantos@hotmail.com>
2025-08-16 09:30:55 -0300

    CADASTRO - ENDEREÇO

--- sharks/src/app/Cadastro/page.tsx
+++ sharks/src/app/Cadastro/page.tsx
@@ -16,6 +16,7 @@ export default function Cadastro() {
                 type='text'
                 id='RAZAO_SOCIAL' 
                 name='RAZAO_SOCIAL'
+                max={100}
                 required
               />  
             </li>
@@ -25,7 +26,8 @@ export default function Cadastro() {
               <input 
                 type="text" 
                 id="NOME_FANTASIA" 
-                name="NOME_FANTASIA" 
+                name="NOME_FANTASIA"
+                max={100} 
               />
             </li>
 
@@ -37,6 +39,7 @@ export default function Cadastro() {
                 name="CNPJ" 
                 pattern="\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}"
                 title="Formato: 00.000.000/0000-00"
+                maxLength={18}
                 required
               />
             </li>
@@ -48,6 +51,7 @@ export default function Cadastro() {
                 id="INSCRICAO_ESTADUAL" 
                 name="INSCRICAO_ESTADUAL"
                 pattern="\d{3}\.\d{3}\.\d{3}\.\d{3}"
+                maxLength={20}
                 required
               />
             </li>
@@ -66,7 +70,112 @@ export default function Cadastro() {
             </li>
           </ul>
         </fieldset>
-        
+ 
+        <fieldset>
+          <legend>Enderço da Empresa</legend>
+          <ul>
+            <li>
+              <label htmlFor='CEP'>CEP: </label>
+              <input
+                type='text'
+                id='CEP' 
+                name='CEP'
+                pattern="\d{5}-\d{3}"
+                title="Formato: 00000-000"
+                maxLength={9}
+                required
+              />
+            </li>
+
+            <li>
+              <label htmlFor='ENDERECO'>Endereço: </label>
+              <input
+                type='text'
+                id='ENDERECO' 
+                name='ENDERECO'
+                max={100}
+                required
+              />
+            </li>
+
+            <li>
+              <label htmlFor='NUMERO'>Número: </label>
+              <input
+                type='text'
+                id='NUMERO' 
+                name='NUMERO'
+                maxLength={10}
+                required
+              />
+            </li>
+
+            <li>
+              <label htmlFor='COMPLEMENTO'>Complemento: </label>
+              <input
+                type='text'
+                id='COMPLEMENTO' 
+                name='COMPLEMENTO'
+                max={50}
+              />
+            </li>
+
+            <li>
+              <label htmlFor='BAIRRO'>Bairro: </label>
+              <input
+                type='text'
+                id='BAIRRO' 
+                name='BAIRRO'
+                max={50}
+                required
+              />
+            </li>
+
+            <li>
+              <label htmlFor='CIDADE'>Cidade: </label>
+              <input
+                type='text'
+                id='CIDADE' 
+                name='CIDADE'
+                max={50}
+                required
+              />
+            </li>
+
+            <li>
+              <label htmlFor='ESTADO'>Estado: </label>
+              <select id='ESTADO' name='ESTADO' required>
+                <option value="">Selecione</option>
+                <option value="AC">Acre</option>
+                <option value="AL">Alagoas</option>
+                <option value="AP">Amapá</option>
+                <option value="AM">Amazonas</option>
+                <option value="BA">Bahia</option>
+                <option value="CE">Ceará</option>
+                <option value="DF">Distrito Federal</option>
+                <option value="ES">Espírito Santo</option>
+                <option value="GO">Goiás</option>
+                <option value="MA">Maranhão</option>
+                <option value="MT">Mato Grosso</option>
+                <option value="MS">Mato Grosso do Sul</option>
+                <option value="MG">Minas Gerais</option>
+                <option value="PA">Pará</option>
+                <option value="PB">Paraíba</option>
+                <option value="PR">Paraná</option>
+                <option value="PE">Pernambuco</option>
+                <option value="PI">Piauí</option>
+                <option value="RJ">Rio de Janeiro</option>
+                <option value="RN">Rio Grande do Norte</option>
+                <option value="RS">Rio Grande do Sul</option>
+                <option value="RO">Rondônia</option>
+                <option value="RR">Roraima</option>
+                <option value="SC">Santa Catarina</option>
+                <option value="SP">São Paulo</option>
+                <option value="SE">Sergipe</option>
+                <option value="TO">Tocantins</option>
+              </select>
+            </li> 
+          </ul> 
+        </fieldset>
       </form>
       
       <p>Já tem uma conta? <Link href="/Login">Faça login aqui!</Link></p>
